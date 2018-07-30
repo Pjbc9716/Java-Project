82d82a65e524b09c271bb79e1c4f4fc3e3888f37
Pablo B <pbustosc97@gmail.com>
2018-07-29 23:29:03 -0600

    Mejora #2

--- Project/src/project/Project.java
+++ Project/src/project/Project.java
@@ -20,8 +20,7 @@ public class Project {
         Administrador myadministrador = new Administrador();
         myadministrador.fillQuestioList();
         myadministrador.fillUserList();
-        System.out.println("Hola");
-        System.out.println("Adios");
+;
         Menu mymenu = new Menu (myadministrador);
         mymenu.setVisible(true);
     }
